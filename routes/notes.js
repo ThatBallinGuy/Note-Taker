85e3e67ba67add910f9711cb4781ebe9d322912c
BallinGuy <tballin2000@hotmail.com>
2022-11-07 16:02:29 -0500

    notes.push working

--- routes/notes.js
+++ routes/notes.js
@@ -1,10 +1,24 @@
 const notes = require('express').Router();
 const { readFromFile, readAndAppend, writeToFile } = require('../helpers/fsUtils');
 const uuid = require('../helpers/uuid');
-const noteData = require('../db/db.json');
 
 notes.get('/', (req, res) => {
     readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
 });
+
+notes.post('/', (req, res) => {
+    const { title, text } = req.body;
+
+    const newNote = {
+        title,
+        text,
+        id: uuid(),
+    };
+
+    readAndAppend(newNote, './db/db.json');
+    res.json('Successfully Posted');
+  
+});
+
 
 module.exports = notes;
